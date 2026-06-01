2d1b703f7e96a338d1568191faa3102e318b22fc
Benjamín <benjamin.diaze@outlook.com>
2026-05-31 20:48:35 -0400

    Initial commit

---
+++ src/TanatosCognitoTriggerCdk/TanatosCognitoTriggerCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TanatosCognitoTriggerCdk
+{
+    public class TanatosCognitoTriggerCdkStack : Stack
+    {
+        internal TanatosCognitoTriggerCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
