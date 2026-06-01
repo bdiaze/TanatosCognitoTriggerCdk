897288b9d66fea96b211c3e148e79c34ad04ed84
Benjamín <benjamin.diaze@outlook.com>
2026-06-01 01:04:55 -0400

    Se cambia inicialización de variables en SuscripcionDao a uso de Lazy con ejecución de consultas en paralelo.

--- TanatosCognitoTrigger/TanatosCognitoTrigger/Repositories/SuscripcionDao.cs
+++ TanatosCognitoTrigger/TanatosCognitoTrigger/Repositories/SuscripcionDao.cs
@@ -8,26 +8,46 @@ using TanatosCognitoTrigger.Helpers;
 
 namespace TanatosCognitoTrigger.Repositories {
 	internal class SuscripcionDao(VariableEntornoHelper variableEntorno, ParameterStoreHelper parameterStore, SecretManagerHelper secretManagerHelper, ClientCredentialsHelper clientCredentialsHelper) {
-		private readonly string _baseUrl = parameterStore.ObtenerParametro(variableEntorno.Obtener("ARN_PARAMETER_TANATOS_API_URL")).Result;
-		private readonly string _cognitoBaseUrl = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["CognitoBaseUrl"];
-		private readonly string _cognitoTriggerClientId = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["CognitoTriggerUserPoolClientId"];
-		private readonly string _cognitoTriggerClientSecret = JsonSerializer.Deserialize<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API")).Result)!["CognitoTriggerUserPoolClientSecret"];
-
 		private readonly string[] _cognitoTriggerScopes = [
 			"api/suscripciones.read.all",
 			"api/suscripciones.write.all",
 			"api/sistema.read.public",
 		];
 
+		private readonly Lazy<Task<ApiConfig>> _config = new(() => InicializarApiConfig(variableEntorno, parameterStore, secretManagerHelper));
+
+		private static async Task<ApiConfig> InicializarApiConfig(VariableEntornoHelper variableEntorno, ParameterStoreHelper parameterStore, SecretManagerHelper secretManagerHelper) {
+			Task<string> taskParametro = parameterStore.ObtenerParametro(variableEntorno.Obtener("ARN_PARAMETER_TANATOS_API_URL"));
+			Task<string> taskSecreto = secretManagerHelper.ObtenerSecreto(variableEntorno.Obtener("ARN_SECRET_TANATOS_API"));
+
+			await Task.WhenAll(taskParametro, taskSecreto);
+			string baseUrl = taskParametro.Result;
+			Dictionary<string, string> secretos = JsonSerializer.Deserialize<Dictionary<string, string>>(taskSecreto.Result)!;
+
+			return new ApiConfig { 
+				BaseUrl = baseUrl,
+				CognitoBaseUrl = secretos["CognitoBaseUrl"],
+				CognitoTriggerClientId = secretos["CognitoTriggerUserPoolClientId"],
+				CognitoTriggerClientSecret = secretos["CognitoTriggerUserPoolClientSecret"],
+			};
+		}
+
 		public async Task ActivarSuscripcionGratuita(string sub) {
+			ApiConfig config = await _config.Value;
+
 			using HttpClient client = new(new RetryHandler(new HttpClientHandler()));
-			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await clientCredentialsHelper.ObtenerAccessToken(_cognitoBaseUrl, _cognitoTriggerClientId, _cognitoTriggerClientSecret, _cognitoTriggerScopes));
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await clientCredentialsHelper.ObtenerAccessToken(
+				config.CognitoBaseUrl,
+				config.CognitoTriggerClientId, 
+				config.CognitoTriggerClientSecret, 
+				_cognitoTriggerScopes
+			));
 
 			EntSuscripcionActivarSuscripcionGratuita entrada = new() { 
 				Sub = sub
 			};
 
-			HttpResponseMessage response = await client.PostAsync(_baseUrl + "/Suscripcion/ActivarSuscripcionGratuita", new StringContent(JsonSerializer.Serialize(entrada), Encoding.UTF8, "application/json"));
+			HttpResponseMessage response = await client.PostAsync(config.BaseUrl + "/Suscripcion/ActivarSuscripcionGratuita", new StringContent(JsonSerializer.Serialize(entrada), Encoding.UTF8, "application/json"));
 			if (!response.IsSuccessStatusCode) {
 				throw new HttpRequestException(
 					$"Ocurrió un error al activar la suscripción gratuita. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}",
@@ -36,5 +56,12 @@ namespace TanatosCognitoTrigger.Repositories {
 				);
 			}
 		}
+	}
+
+	internal class ApiConfig {
+		public required string BaseUrl { get; init; }
+		public required string CognitoBaseUrl { get; init; }
+		public required string CognitoTriggerClientId { get; init; }
+		public required string CognitoTriggerClientSecret { get; init; }
 	}
 }
